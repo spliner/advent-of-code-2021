800401e6a56bbe8f431cda7e536229c6b9001065
Fillipe Rosini <hi@frosini.dev>
2021-12-01 12:29:24 -0300

    Day 1, part 2 in F#

--- fsharp/AdventOfCode2021.Tests/Day1Tests.fs
+++ fsharp/AdventOfCode2021.Tests/Day1Tests.fs
@@ -2,9 +2,47 @@ module Tests
 
 open Xunit
 
-let measurements = [ 199; 200; 208; 210; 200; 207; 240; 269; 260; 263; ]
-
 [<Fact>]
 let ``Count measurements should return 7 for example input`` () =
+    let measurements = [ 199; 200; 208; 210; 200; 207; 240; 269; 260; 263; ]
     let count = Day1.countMeasurements measurements
     Assert.Equal(7, count);
+
+[<Fact>]
+let ``Parse windows should return 8 windows`` () =
+    let measurements = [ 199; 200; 208; 210; 200; 207; 240; 269; 260; 263; ]
+    let windows = Day1.parseWindows measurements
+
+    let expected = [
+        Day1.SlidingWindow(199, 200, 208)
+        Day1.SlidingWindow(200, 208, 210)
+        Day1.SlidingWindow(208, 210, 200)
+        Day1.SlidingWindow(210, 200, 207)
+        Day1.SlidingWindow(200, 207, 240)
+        Day1.SlidingWindow(207, 240, 269)
+        Day1.SlidingWindow(240, 269, 260)
+        Day1.SlidingWindow(269, 260, 263)
+    ]
+
+    Assert.Equal<Day1.SlidingWindow>(expected, windows);
+
+[<Fact>]
+let ``Sum of a sliding window should return correct value`` () =
+    let window = Day1.SlidingWindow(199, 200, 208)
+    let sum = Day1.sum window
+    Assert.Equal(607, sum)
+
+[<Fact>]
+let ``Count window measurements should return 5 for example input`` () =
+    let windows = [
+        Day1.SlidingWindow(199, 200, 208)
+        Day1.SlidingWindow(200, 208, 210)
+        Day1.SlidingWindow(208, 210, 200)
+        Day1.SlidingWindow(210, 200, 207)
+        Day1.SlidingWindow(200, 207, 240)
+        Day1.SlidingWindow(207, 240, 269)
+        Day1.SlidingWindow(240, 269, 260)
+        Day1.SlidingWindow(269, 260, 263)
+    ]
+    let measurements = Day1.countWindowMeasurements windows
+    Assert.Equal(5, measurements);
--- fsharp/AdventOfCode2021/Day1.fs
+++ fsharp/AdventOfCode2021/Day1.fs
@@ -1,10 +1,11 @@
 ﻿module Day1
 
+type SlidingWindow = int * int * int
+
 let parseInput (input: string) =
     input.Split('\n')
         |> Seq.map (fun s -> s.Trim())
         |> Seq.map int
-        |> Seq.toList
 
 let countMeasurements (measurements: int list) =
     let rec countMeasurements' previous list =
@@ -16,10 +17,26 @@ let countMeasurements (measurements: int list) =
     countMeasurements' measurements.Head measurements.Tail
 
 let partOne (input: string): Result<string, string> =
-    let measurements = parseInput input
+    let measurements = parseInput input |> Seq.toList
     match measurements with
     | _head :: _tail -> Ok (string (countMeasurements measurements))
     | _ -> Error "Invalid input"
 
+let parseWindows (input: int list) =
+    Seq.windowed 3 input
+        |> Seq.map (fun array -> SlidingWindow (array.[0], array.[1], array.[2]))
+        |> Seq.toList
+
+let sum (window: SlidingWindow) =
+    let (x, y, z) = window
+    x + y + z
+
+let countWindowMeasurements (windows: SlidingWindow list) =
+    let sums = windows |> List.map sum
+    countMeasurements sums
+
 let partTwo (input: string): Result<string, string> =
-    Ok $"Part two got input {input}!"
+    let windows = parseInput input |> Seq.toList |> parseWindows
+    match windows with
+    | _head :: _tail -> Ok (string (countWindowMeasurements windows))
+    | _ -> Error "Invalid input"
