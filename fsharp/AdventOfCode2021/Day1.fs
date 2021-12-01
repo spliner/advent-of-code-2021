fbec8109874ef8695dae6caf963d229a104cde16
Fillipe Rosini <hi@frosini.dev>
2021-11-30 23:42:45 -0300

    Day 1 preparations

---
+++ fsharp/AdventOfCode2021/Day1.fs
@@ -0,0 +1,7 @@
+﻿module Day1
+
+let partOne (input: string): Result<string, string> =
+    Ok $"Part one got input {input}!"
+
+let partTwo (input: string): Result<string, string> =
+    Ok $"Part two got input {input}!"
--- fsharp/AdventOfCode2021/Program.fs
+++ fsharp/AdventOfCode2021/Program.fs
@@ -1,4 +1,40 @@
-﻿
+﻿open System
+open System.IO
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+let (>=>) f1 f2 arg =
+    match f1 arg with
+    | Ok data -> f2 data
+    | Error e -> Error e
+
+type Argument = { Day: int; Part: int; Input: string }
+
+let parseArgs () =
+    let args = Environment.GetCommandLineArgs()
+
+    if Array.length args = 4 then
+        let path = args.[3]
+        let input = File.ReadAllText path
+
+        Ok
+            { Day = int args.[1]
+              Part = int args.[2]
+              Input = input }
+    else
+        Error "Invalid number of arguments"
+
+let invokeRunner argument =
+    let getRunner argument =
+        match (argument.Day, argument.Part) with
+        | 1, 1 -> Ok(Day1.partOne)
+        | 1, 2 -> Ok(Day1.partTwo)
+        | day, part -> Error $"Could not find a runner for day {day} part {part}"
+
+    match getRunner argument with
+    | Ok runner -> runner argument.Input
+    | Error e -> Error e
+
+let getResult = parseArgs >=> invokeRunner
+
+match getResult () with
+| Ok r -> printf $"Success: {r}"
+| Error e -> printf $"Error: {e}"
