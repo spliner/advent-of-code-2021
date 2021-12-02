2145204accd051c7edb999c65254d9b2177e7ff9
Fillipe Rosini <hi@frosini.dev>
2021-12-02 14:26:30 -0300

    Day 2, part 1 in F#

---
+++ fsharp/AdventOfCode2021.Tests/Day2Tests.fs
@@ -0,0 +1,121 @@
+﻿module Day2Tests
+
+open Xunit
+
+[<Fact>]
+let ``Parse forward command should correctly parse`` () =
+    let rawCommand = "forward 12"
+    let command = Day2.parseCommand rawCommand
+    Assert.Equal(Ok(Day2.Forward(12)), command)
+
+[<Fact>]
+let ``Parse down command should correctly parse`` () =
+    let rawCommand = "down 10"
+    let command = Day2.parseCommand rawCommand
+    Assert.Equal(Ok(Day2.Down(10)), command)
+
+[<Fact>]
+let ``Parse up command should correctly parse`` () =
+    let rawCommand = "up 5"
+    let command = Day2.parseCommand rawCommand
+    Assert.Equal(Ok(Day2.Up(5)), command)
+
+[<Fact>]
+let ``Parse invalid string should return error`` () =
+    let rawCommand = "up 5 123"
+    let command = Day2.parseCommand rawCommand
+    Assert.Equal(Error("Invalid command: up 5 123"), command)
+
+[<Fact>]
+let ``Parse invalid command name should return error`` () =
+    let rawCommand = "foo 5"
+    let command = Day2.parseCommand rawCommand
+    Assert.Equal(Error("Invalid command name: foo"), command)
+
+[<Fact>]
+let ``Parse example input should correctly parse`` () =
+    let rawInput =
+        "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2"
+
+    let parsedInput = Day2.parseInput rawInput
+
+    let expected =
+        [ Day2.Forward 5
+          Day2.Down 5
+          Day2.Forward 8
+          Day2.Up 3
+          Day2.Down 8
+          Day2.Forward 2 ]
+
+    Assert.Equal<Day2.Command>(expected, parsedInput)
+
+[<Fact>]
+let ``Parse raw input with invalid commands should return only valid commands`` () =
+    let rawInput =
+        "forward2 5
+down 5 123
+forward
+up 3
+down 8
+forward 2"
+
+    let parsedInput = Day2.parseInput rawInput
+
+    let expected =
+        [ Day2.Up 3
+          Day2.Down 8
+          Day2.Forward 2 ]
+
+    Assert.Equal<Day2.Command>(expected, parsedInput)
+
+[<Fact>]
+let ``Next state with Forward command adds units to position`` () =
+    let state = { Day2.Position = 10; Day2.Depth = 10 }
+    let command = Day2.Forward 10
+
+    let nextState = Day2.nextState state command
+
+    let expected = { Day2.Position = 20; Day2.Depth = 10 }
+
+    Assert.Equal(expected, nextState)
+
+[<Fact>]
+let ``Next state with Up command subtracts units from depth`` () =
+    let state = { Day2.Position = 10; Day2.Depth = 10 }
+    let command = Day2.Up 10
+
+    let nextState = Day2.nextState state command
+
+    let expected = { Day2.Position = 10; Day2.Depth = 0 }
+
+    Assert.Equal(expected, nextState)
+
+[<Fact>]
+let ``Next state with Down command adds units to depth`` () =
+    let state = { Day2.Position = 10; Day2.Depth = 10 }
+    let command = Day2.Down 10
+
+    let nextState = Day2.nextState state command
+
+    let expected = { Day2.Position = 10; Day2.Depth = 20 }
+
+    Assert.Equal(expected, nextState)
+
+[<Fact>]
+let ``Run course for example input should return 150`` () =
+    let commands =
+        [ Day2.Forward 5
+          Day2.Down 5
+          Day2.Forward 8
+          Day2.Up 3
+          Day2.Down 8
+          Day2.Forward 2 ]
+
+    let result = Day2.runCourse commands
+
+    Assert.Equal(150, result)
--- fsharp/AdventOfCode2021/Day2.fs
+++ fsharp/AdventOfCode2021/Day2.fs
@@ -1,7 +1,61 @@
 ﻿module Day2
 
-let partOne (input: string): Result<string, string> =
-    Ok $"Got {input}!"
+type Command =
+    | Forward of int
+    | Down of int
+    | Up of int
 
-let partTwo (input: string): Result<string, string> =
-    Ok $"Got {input}!"
+type State = { Position: int; Depth: int }
+
+let parseCommand (rawCommand: string) =
+    let split = rawCommand.Split(' ')
+
+    if Array.length split <> 2 then
+        Error $"Invalid command: {rawCommand}"
+    else
+        let commandName = split.[0]
+        let unit = int split.[1]
+
+        match commandName with
+        | "forward" -> Ok(Forward(unit))
+        | "down" -> Ok(Down(unit))
+        | "up" -> Ok(Up(unit))
+        | _ -> Error $"Invalid command name: {commandName}"
+
+let parseInput (input: string) =
+    input.Trim().Split('\n')
+    |> Seq.map (fun s -> s.Trim())
+    |> Seq.map parseCommand
+    |> Seq.fold
+        (fun list command ->
+            match command with
+            | Ok c -> list @ [ c ]
+            | _ -> list)
+        []
+    |> Seq.toList
+
+let nextState state command =
+    match command with
+    | Forward units ->
+        { state with
+              Position = state.Position + units }
+    | Up units ->
+        { state with
+              Depth = state.Depth - units }
+    | Down units ->
+        { state with
+              Depth = state.Depth + units }
+
+let runCourse (commands: Command list) =
+    let finalState =
+        commands
+        |> Seq.fold nextState { Position = 0; Depth = 0 }
+
+    finalState.Position * finalState.Depth
+
+let partOne (input: string) : Result<string, string> =
+    let commands = parseInput(input)
+    let result = runCourse commands
+    Ok $"{result}"
+
+let partTwo (input: string) : Result<string, string> = Ok $"Got {input}!"
