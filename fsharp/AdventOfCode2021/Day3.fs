5719df99a1dfb777a54c1940d5cb1f3bf76db471
Fillipe Rosini <hi@frosini.dev>
2021-12-03 18:48:41 -0300

    Initial Day 2 part 1 implementation

--- fsharp/AdventOfCode2021/Day3.fs
+++ fsharp/AdventOfCode2021/Day3.fs
@@ -1,5 +1,40 @@
 ﻿module Day3
 
+type Bit =
+    | Zero
+    | One
+
+let parseBit character =
+    match character with
+    | '0' -> Ok Zero
+    | '1' -> Ok One
+    | _ -> Error $"Invalid character: {character}"
+
+let parseLine (line: string) =
+    line
+    |> Seq.map parseBit
+    |> Seq.fold
+        (fun list bitResult ->
+            match bitResult with
+            | Ok bit -> list @ [ bit ]
+            | Error _ -> list)
+        []
+    |> Seq.toList
+
+let parseInput (input: string) =
+    StringUtils.splitLines input
+    |> Seq.map parseLine
+    |> Seq.toList
+
+let getMostCommonBit (input: Bit list list) (column: int) =
+    let bits =
+        input
+        |> Seq.map (fun row -> row.[column])
+        |> Seq.groupBy id
+        |> Seq.sortByDescending (fun (_, group) -> Seq.length group)
+        |> Seq.toList
+    bits.[0]
+
 let partOne (input: string) : Result<string, string> = Ok ""
 
 let partTwo (input: string) : Result<string, string> = Ok ""
