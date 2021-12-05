b4e42e53733952221607a283582a9df8358f0d80
Fillipe Rosini <hi@frosini.dev>
2021-12-04 21:56:13 -0300

    Day 3, part 1 in F#

--- fsharp/AdventOfCode2021.Tests/Day3Tests.fs
+++ fsharp/AdventOfCode2021.Tests/Day3Tests.fs
@@ -1,3 +1,58 @@
 ﻿module Day3Tests
 
 open Xunit
+
+let input =
+    "00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010"
+
+let expectedParsedInput =
+    [ [ 0; 0; 1; 0; 0 ]
+      [ 1; 1; 1; 1; 0 ]
+      [ 1; 0; 1; 1; 0 ]
+      [ 1; 0; 1; 1; 1 ]
+      [ 1; 0; 1; 0; 1 ]
+      [ 0; 1; 1; 1; 1 ]
+      [ 0; 0; 1; 1; 1 ]
+      [ 1; 1; 1; 0; 0 ]
+      [ 1; 0; 0; 0; 0 ]
+      [ 1; 1; 0; 0; 1 ]
+      [ 0; 0; 0; 1; 0 ]
+      [ 0; 1; 0; 1; 0 ] ]
+
+[<Fact>]
+let ``Parse example input should return int matrix`` () =
+    let parsedInput = Day3.parseInput input
+    Assert.Equal<int list>(expectedParsedInput, parsedInput)
+
+[<Theory>]
+[<InlineData(0, 1)>]
+[<InlineData(1, 0)>]
+[<InlineData(2, 1)>]
+[<InlineData(3, 1)>]
+[<InlineData(4, 0)>]
+let ``Most common bit of example input column should match expected value`` (column: int, expected: int) =
+    let mostCommon =
+        Day3.getMostCommonBit expectedParsedInput column
+
+    Assert.Equal(expected, mostCommon)
+
+[<Fact>]
+let ``Gamma rate of example input should be 22`` () =
+    let gammaRate = Day3.getGammaRate expectedParsedInput
+    Assert.Equal(22, gammaRate)
+
+[<Fact>]
+let ``Epsilon rate of example input should be 9`` () =
+    let gammaRate = Day3.getEpsilonRate expectedParsedInput
+    Assert.Equal(9, gammaRate)
--- fsharp/AdventOfCode2021/Day3.fs
+++ fsharp/AdventOfCode2021/Day3.fs
@@ -1,40 +1,46 @@
 ﻿module Day3
 
-type Bit =
-    | Zero
-    | One
-
-let parseBit character =
-    match character with
-    | '0' -> Ok Zero
-    | '1' -> Ok One
-    | _ -> Error $"Invalid character: {character}"
-
-let parseLine (line: string) =
-    line
-    |> Seq.map parseBit
-    |> Seq.fold
-        (fun list bitResult ->
-            match bitResult with
-            | Ok bit -> list @ [ bit ]
-            | Error _ -> list)
-        []
-    |> Seq.toList
+open System
 
 let parseInput (input: string) =
     StringUtils.splitLines input
-    |> Seq.map parseLine
+    |> Seq.map
+        (fun line ->
+            line
+            |> Seq.map (fun c -> int c - int '0')
+            |> Seq.toList)
     |> Seq.toList
 
-let getMostCommonBit (input: Bit list list) (column: int) =
-    let bits =
-        input
-        |> Seq.map (fun row -> row.[column])
-        |> Seq.groupBy id
-        |> Seq.sortByDescending (fun (_, group) -> Seq.length group)
-        |> Seq.toList
-    bits.[0]
+let getMostCommonBit (input: int list list) (column: int) : int =
+    input
+    |> Seq.map (fun row -> row.[column])
+    |> Seq.groupBy id
+    |> Seq.sortByDescending (fun (_, group) -> Seq.length group)
+    |> Seq.map fst
+    |> Seq.head
 
-let partOne (input: string) : Result<string, string> = Ok ""
+let getGammaRate (input: int list list) =
+    let rate =
+        { 0 .. (Seq.length input.[0]) - 1 }
+        |> Seq.map (getMostCommonBit input)
+        |> Seq.fold (fun acc bit -> acc + string bit) ""
+
+    Convert.ToInt32(rate, 2)
+
+let getEpsilonRate (input: int list list) =
+    let gammaRate = getGammaRate input
+
+    let mask =
+        { 0 .. (Seq.length input.[0]) - 1 }
+        |> Seq.fold (fun acc _ -> acc <<< 1 ||| 0b1) 0b0
+
+    ~~~gammaRate &&& mask
+
+let partOne (input: string) : Result<string, string> =
+    let parsedInput = parseInput input
+    let gammaRate = getGammaRate parsedInput
+    let epsilonRate = getEpsilonRate parsedInput
+    let powerConsumption = gammaRate * epsilonRate
+    Ok(string powerConsumption)
 
 let partTwo (input: string) : Result<string, string> = Ok ""
