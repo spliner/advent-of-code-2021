3b55571eaf502ef42e7168582b68a6acb1280dd7
Fillipe Rosini <hi@frosini.dev>
2021-12-04 23:02:07 -0300

    Finished Day 3, part 2 in F#

--- fsharp/AdventOfCode2021/Day3.fs
+++ fsharp/AdventOfCode2021/Day3.fs
@@ -11,29 +11,23 @@ let parseInput (input: string) =
             |> Seq.toList)
     |> Seq.toList
 
-let getMostCommonBit (input: int list list) (column: int) : int =
+let private getFirstSortedBit sortBy (input: int list list) (column: int) (defaultValue: int) : int =
     let groups =
         input
         |> Seq.map (fun row -> row.[column])
         |> Seq.groupBy id
-        |> Seq.sortByDescending (fun (_, group) -> Seq.length group)
+        |> sortBy (fun (_, group) -> Seq.length group)
         |> Seq.toList
 
     match groups with
-    | [ _, group1; _, group2 ] when Seq.length group1 = Seq.length group2 -> 1
+    | [ _, group1; _, group2 ] when Seq.length group1 = Seq.length group2 -> defaultValue
     | _ -> groups |> Seq.map fst |> Seq.head
+
+let getMostCommonBit (input: int list list) (column: int) : int =
+    getFirstSortedBit Seq.sortByDescending input column 1
 
 let getLeastCommonBit (input: int list list) (column: int) : int =
-    let groups =
-        input
-        |> Seq.map (fun row -> row.[column])
-        |> Seq.groupBy id
-        |> Seq.sortBy (fun (_, group) -> Seq.length group)
-        |> Seq.toList
-
-    match groups with
-    | [ _, group1; _, group2 ] when Seq.length group1 = Seq.length group2 -> 0
-    | _ -> groups |> Seq.map fst |> Seq.head
+    getFirstSortedBit Seq.sortBy input column 0
 
 let getGammaRate (input: int list list) =
     let rate =
@@ -59,12 +53,12 @@ let partOne (input: string) : Result<string, string> =
     let powerConsumption = gammaRate * epsilonRate
     Ok(string powerConsumption)
 
-let getOxygenGeneratorRating (input: int list list) =
+let private getRating getFirstSortedBit (input: int list list) =
     let rec getRating (remainingInputs: int list list) (column: int) =
         match remainingInputs with
         | [ answer ] -> answer
         | _ ->
-            let mostCommon = getMostCommonBit remainingInputs column
+            let mostCommon = getFirstSortedBit remainingInputs column
 
             let matches =
                 remainingInputs
@@ -79,29 +73,16 @@ let getOxygenGeneratorRating (input: int list list) =
 
     Convert.ToInt32(rating, 2)
 
-let getCO2ScrubberRating (input: int list list) =
-    let rec getRating (remainingInputs: int list list) (column: int) =
-        match remainingInputs with
-        | [ answer ] -> answer
-        | _ ->
-            let mostCommon = getLeastCommonBit remainingInputs column
+let getOxygenGeneratorRating (input: int list list) = getRating getMostCommonBit input
 
-            let matches =
-                remainingInputs
-                |> Seq.filter (fun line -> line.[column] = mostCommon)
-                |> Seq.toList
-
-            getRating matches (column + 1)
-
-    let rating =
-        getRating input 0
-        |> Seq.fold (fun acc bit -> acc + string bit) ""
-
-    Convert.ToInt32(rating, 2)
+let getCO2ScrubberRating (input: int list list) = getRating getLeastCommonBit input
 
 let partTwo (input: string) : Result<string, string> =
     let parsedInput = parseInput input
     let oxygenGeneratorRating = getOxygenGeneratorRating parsedInput
     let co2ScrubberRating = getCO2ScrubberRating parsedInput
-    let lifeSupportRating = oxygenGeneratorRating * co2ScrubberRating
+
+    let lifeSupportRating =
+        oxygenGeneratorRating * co2ScrubberRating
+
     Ok(string lifeSupportRating)
