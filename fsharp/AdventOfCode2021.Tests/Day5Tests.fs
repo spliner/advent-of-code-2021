c2ebe8df2ef57e25aeece6f584a3c2f6f1c52714
Fillipe Rosini <hi@frosini.dev>
2021-12-07 23:24:40 -0300

    Day 5, part 1 in F#

--- fsharp/AdventOfCode2021.Tests/Day5Tests.fs
+++ fsharp/AdventOfCode2021.Tests/Day5Tests.fs
@@ -1,3 +1,109 @@
 ﻿module Day5Tests
 
 open Xunit
+
+type AllPoints() =
+    static member TestData: obj [] list =
+        [ [| Day5.Point(0, 9)
+             Day5.Point(5, 9)
+             Set.ofList [ Day5.Point(0, 9)
+                          Day5.Point(1, 9)
+                          Day5.Point(2, 9)
+                          Day5.Point(3, 9)
+                          Day5.Point(4, 9)
+                          Day5.Point(5, 9) ] |]
+          [| Day5.Point(9, 4)
+             Day5.Point(3, 4)
+             Set.ofList [ Day5.Point(3, 4)
+                          Day5.Point(4, 4)
+                          Day5.Point(5, 4)
+                          Day5.Point(6, 4)
+                          Day5.Point(7, 4)
+                          Day5.Point(8, 4)
+                          Day5.Point(9, 4) ] |]
+
+          [| Day5.Point(1, 1)
+             Day5.Point(1, 3)
+             Set.ofList [ Day5.Point(1, 1)
+                          Day5.Point(1, 2)
+                          Day5.Point(1, 3) ] |] ]
+
+    [<Theory>]
+    [<MemberData(nameof AllPoints.TestData)>]
+    member _.``Create line should contain all points`` startPoint endPoint expectedPoints =
+        let line = Day5.Line.create startPoint endPoint
+
+        Assert.Equal(startPoint, line.Start)
+        Assert.Equal(endPoint, line.End)
+
+        Assert.Equal<Day5.Point>(expectedPoints, line.All)
+
+[<Fact>]
+let ``Parse example input should return correct points`` () =
+    let input =
+        "0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2"
+
+    let parsedInput = Day5.parseInput input
+
+    let points =
+        parsedInput
+        |> List.map (fun line -> line.Start, line.End)
+
+    let expectedPoints =
+        [ Day5.Point(0, 9), Day5.Point(5, 9)
+          Day5.Point(8, 0), Day5.Point(0, 8)
+          Day5.Point(9, 4), Day5.Point(3, 4)
+          Day5.Point(2, 2), Day5.Point(2, 1)
+          Day5.Point(7, 0), Day5.Point(7, 4)
+          Day5.Point(6, 4), Day5.Point(2, 0)
+          Day5.Point(0, 9), Day5.Point(2, 9)
+          Day5.Point(3, 4), Day5.Point(1, 4)
+          Day5.Point(0, 0), Day5.Point(8, 8)
+          Day5.Point(5, 5), Day5.Point(8, 2) ]
+
+    Assert.Equal<Day5.Point * Day5.Point>(expectedPoints, points)
+
+[<Fact>]
+let ``Get intersection should return expected value`` () =
+    let line1 =
+        Day5.Line.create (Day5.Point(0, 9)) (Day5.Point(5, 9))
+
+    let line2 =
+        Day5.Line.create (Day5.Point(0, 9)) (Day5.Point(2, 9))
+
+    let intersection = Day5.Line.getIntersection line1 line2
+
+    let expectedIntersection =
+        Seq.ofList [ Day5.Point(0, 9)
+                     Day5.Point(1, 9)
+                     Day5.Point(2, 9) ]
+
+    Assert.Equal<Day5.Point>(expectedIntersection, intersection)
+
+[<Fact>]
+let ``Count overlaps of example input should return 5`` () =
+    let lines =
+        [ Day5.Point(0, 9), Day5.Point(5, 9)
+          Day5.Point(8, 0), Day5.Point(0, 8)
+          Day5.Point(9, 4), Day5.Point(3, 4)
+          Day5.Point(2, 2), Day5.Point(2, 1)
+          Day5.Point(7, 0), Day5.Point(7, 4)
+          Day5.Point(6, 4), Day5.Point(2, 0)
+          Day5.Point(0, 9), Day5.Point(2, 9)
+          Day5.Point(3, 4), Day5.Point(1, 4)
+          Day5.Point(0, 0), Day5.Point(8, 8)
+          Day5.Point(5, 5), Day5.Point(8, 2) ]
+        |> List.map (fun (startPoint, endPoint) -> Day5.Line.create startPoint endPoint)
+
+    let overlaps = Day5.countOverlaps lines
+
+    Assert.Equal(5, overlaps)
--- fsharp/AdventOfCode2021/Day5.fs
+++ fsharp/AdventOfCode2021/Day5.fs
@@ -1,5 +1,86 @@
 ﻿module Day5
 
-let partOne (input: string) : Result<string, string> = Ok ""
+type Point = int * int
+
+module Line =
+    type T =
+        { Start: Point
+          End: Point
+          All: Point Set }
+
+    let private isVertical' startPoint endPoint =
+        let x1, _ = startPoint
+        let x2, _ = endPoint
+        x1 = x2
+
+    let isVertical { Start = startPoint; End = endPoint } = isVertical' startPoint endPoint
+
+    let isHorizontal' startPoint endPoint =
+        let _, y1 = startPoint
+        let _, y2 = endPoint
+        y1 = y2
+
+    let isHorizontal { Start = startPoint; End = endPoint } = isHorizontal' startPoint endPoint
+
+    let create (startPoint: Point) (endPoint: Point) =
+        let x1, y1 = startPoint
+        let x2, y2 = endPoint
+
+        let allPoints =
+            if isVertical' startPoint endPoint then
+                let startY, endY = if y1 > y2 then y2, y1 else y1, y2
+                [ for y in startY .. endY -> (x1, y) ]
+            elif isHorizontal' startPoint endPoint then
+                let startX, endX = if x1 > x2 then x2, x1 else x1, x2
+                [ for x in startX .. endX -> (x, y1) ]
+            else
+                List.empty
+
+        { Start = startPoint
+          End = endPoint
+          All = Set.ofList allPoints }
+
+    let getIntersection (line1: T) (line2: T) =
+        let { All = allPoints1 } = line1
+        let { All = allPoints2 } = line2
+
+        Set.intersect allPoints1 allPoints2
+
+let parseInput (input: string) =
+    StringUtils.splitLines input
+    |> Seq.map
+        (fun line ->
+            let startPoint, endPoint =
+                match line.Split(" -> ") with
+                | [| p1; p2 |] ->
+                    let getCoordinates (part: string) =
+                        let parts =
+                            part.Split(',') |> Seq.map int |> Seq.toList
+
+                        parts.[0], parts.[1]
+
+                    (getCoordinates p1), (getCoordinates p2)
+                | _ -> (0, 0), (0, 0)
+
+            Line.create startPoint endPoint)
+    |> Seq.toList
+
+let countOverlaps (lines: Line.T list) =
+    lines
+    |> Seq.mapi
+        (fun index line ->
+            lines
+            |> Seq.skip (index + 1)
+            |> Seq.fold
+                (fun acc otherLine ->
+                    let intersection = Line.getIntersection line otherLine
+                    acc + intersection)
+                Set.empty)
+    |> Seq.fold (fun acc points -> acc + points) Set.empty
+    |> Seq.length
+
+let partOne (input: string) : Result<string, string> =
+    let result = parseInput input |> countOverlaps
+    Ok(string result)
 
 let partTwo (input: string) : Result<string, string> = Ok ""
